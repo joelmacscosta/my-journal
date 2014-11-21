c8534649b1cb6fd74fffbb7ecb9f791b1dc709ad
Joelma <joelmacscosta@gmail.com>
2014-11-20 22:48:25 -0200

    Adding relation with articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	has_many :articles, dependent: :destroy
+	has_secure_password
+
+	validates :name,  presence: true, length: {minimum: 5, maximum: 50 }
+    validates :email, presence: true, length: { minimum: 5, maximum: 50 }
+	validates :password, presence: true, length: { minimum: 4, maximum: 15 }
 end
