e5aa1799d6037577925a060f6a795b0012b65a12
Joelma <joelmacscosta@gmail.com>
2014-11-21 08:53:04 -0200

    Create fake data using gem Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+require 'faker'
+
 user = User.new(name: "Joelma Costa", email: "joelmacscosta@gmail.com" , password: "pass", password_confirmation: "pass")
 user.articles.new(title: "My journal idea", content: "Designed to integrate people and culture. Boost social life")
 user.save
+
+15.times do
+	password = Faker::Internet.password
+	user = User.new(name: Faker::Name.name , email: Faker::Internet.email , password: password, password_confirmation: password)
+	user.save
+
+	article = user.articles.new(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph(2, false, 4))
+	article.save  
+
+	article = user.articles.new(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph(2, false, 4))
+	article.save  
+end
