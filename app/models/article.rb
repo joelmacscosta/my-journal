36ab1b14bb8a0e1b258ce12b67b1f50066a61ada
Joelma <joelmacscosta@gmail.com>
2014-11-21 22:04:19 -0200

    Validate association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
+	validates_associated :user
 
 	validates :content,  presence: true, length: {minimum: 10 }
     validates :title, presence: true, length: { minimum: 5, maximum: 50 }
