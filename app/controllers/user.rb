37c2304ade1a40affcab16d5529cac430d5a1b45
Joelma <joelmacscosta@gmail.com>
2014-11-26 23:57:17 -0200

    Create post user controller

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -6,3 +6,15 @@ end
 get '/user/new' do
 	erb :"user/new"
 end
+
+post '/user/new' do
+  user = User.new(name: params[:user_name], 
+  				     email: params[:user_email],
+  				     password: params[:user_password] )
+  if user.valid?
+  	user.save
+  	redirect :"users"
+  else
+  	redirect :"user/new"
+  end
+end
