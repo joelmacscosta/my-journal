1a2d135fbecec247361259b97b37eceb35bdf55a
Joelma <joelmacscosta@gmail.com>
2014-12-03 22:01:26 -0200

    Improve user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	validates :name,  presence: true, length: {minimum: 5, maximum: 50 }
-    validates :email, presence: true, length: { minimum: 5, maximum: 50 }
+    validates :email, presence: true, length: { minimum: 5, maximum: 50 }, format: { with: /.+@.+\..+/i }, uniqueness: true
 	validates :password, presence: true, length: { minimum: 4, maximum: 15 }
+	
 end
