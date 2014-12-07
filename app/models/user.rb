965ac6fc370dc6c0a09b64a71d38d09d9b088eb4
Joelma <joelmacscosta@gmail.com>
2014-12-06 22:43:25 -0200

    Improve user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
 	validates :name,  presence: true, length: {minimum: 5, maximum: 50 }
     validates :email, presence: true, length: { minimum: 5, maximum: 50 }, format: { with: /.+@.+\..+/i }, uniqueness: true
-	validates :password, presence: true, length: { minimum: 4, maximum: 15 }
+	validates :password, presence: true, length: { minimum: 4, maximum: 15 }, :confirmation =>true
+	validates_confirmation_of :password, :message => "should match confirmation"
 	
 end
