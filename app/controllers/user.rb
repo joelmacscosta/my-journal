bae20a7cc057a42a813382586961e66c09f32673
Joelma <joelmacscosta@gmail.com>
2014-12-06 22:40:13 -0200

    Added messages validation to new user

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -4,17 +4,20 @@ get '/users' do
 end
 
 get '/user/new' do
+  @user = User.new
 	erb :"user/new"
 end
 
 post '/user/new' do
-  user = User.new(name: params[:user_name], 
+  @user = User.new(name: params[:user_name], 
   				     email: params[:user_email],
   				     password: params[:user_password] )
-  if user.valid?
-  	user.save
+  if @user.valid?
+  	@user.save
+    flash[:notice] = "Successfully created user."
   	redirect :"users"
   else
+    flash[:error] = @user.errors.full_messages 
   	redirect :"user/new"
   end
 end
