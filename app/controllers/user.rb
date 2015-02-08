29212e7f834cd2e4ee4ba2ec363d34919b2ad5d8
joelmacscosta <joelmacscosta@gmail.com>
2015-02-08 18:39:02 -0200

    Delete not used instance variables

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -4,22 +4,22 @@ get '/users' do
 end
 
 get '/signup' do
-  @user = User.new
+  user = User.new
 	erb :"user/new"
 end
 
 post '/signup' do
-  @user = User.new(name: params[:user_name], 
+  user = User.new(name: params[:user_name], 
   				     email: params[:user_email],
   				     password: params[:user_password],  
                password_confirmation: params[:user_password_confirmation] )
-  if @user.valid?
-  	@user.save
+  if user.valid?
+  	user.save
     flash[:notice] = "Successfully created user."
-    session[:user] = @user.id
+    session[:user] = user.id
   	redirect :"users"
   else
-    flash[:error] = @user.errors.full_messages 
+    flash[:error] = user.errors.full_messages 
   	redirect :"user/new"
   end
 end
